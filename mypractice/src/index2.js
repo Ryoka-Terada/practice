4f4537c9717fa698a721a96fe5bf13beabf51b1c
Ryoka-Terada <77401198+Ryoka-Terada@users.noreply.github.com>
2021-01-28 02:48:45 +0900

    function以降途中

--- mypractice/src/index2.js
+++ mypractice/src/index2.js
@@ -1,12 +1,11 @@
 import { render } from '@testing-library/react';
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Normal from './normal';
-import Index2 from './index2';
 import * as serviceWorker from './serviceWorker';
+import React, { useState,useEffect } from 'react'
 
-class SelectMath extends React.Component{
+class SelectMathaaa extends React.Component{
   constructor(props){
     super(props);
     this.state= {
@@ -35,7 +34,7 @@ class SelectMath extends React.Component{
             <option value="4">４×４</option>
             <option value="5">５×５</option>
           </select>
-          {/* これが可動式マスの部分 */}
+          {/* これが可動式マスの部分  */}
           {this.state.maths}
         </div>
       </div>
@@ -142,21 +141,6 @@ function calculateWinner(squares) {
   }
   bingo.push(diagonalBingo);
 
-  /* ３×３の場合、ビンゴとなるのは以下のどれかの列の中身が全て一致した場合 */
-  // const lines = [
-  //   // よこ
-  //   [0, 1, 2],
-  //   [3, 4, 5],
-  //   [6, 7, 8],
-  //   // たて
-  //   [0, 3, 6],
-  //   [1, 4, 7],
-  //   [2, 5, 8],
-  //   // ななめ
-  //   [0, 4, 8],
-  //   [2, 4, 6],
-  // ];
-
   var bingoResult;
   for (let i = 0; i < bingo.length; i++) {
     var checkBingoLine = [];
@@ -210,7 +194,7 @@ class Board extends React.Component {
   }
 }
 
-class Game extends React.Component {
+class Gameaaa extends React.Component {
   renderBoard(){
     return <Board />;
   }
@@ -220,7 +204,7 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <h2>これが自分で改造したもの</h2>
+          <h2>これがfunctionメソッドで統一を図っているもの</h2>
           {this.renderBoard()}
         </div>
         <div className="game-info">
@@ -232,18 +216,132 @@ class Game extends React.Component {
   }
 }
 
+// function Tmp(props){
+//   console.log("hello "+props.tmp1)
+//   return(
+//     <h1>hello function</h1>
+//   );
+// }
+
+// export default function Game(){
+//   return(
+//     <Tmp tmp1="aaa"/>
+//   );
+// }
+
+
+function RenderSquare(i,squares) {
+  return (
+  // <button className="square" onClick={() => {this.handleClick(i); this.makeBoard(this.state.num, 0)}}>
+  <button className="square">
+    {squares[i]}
+  </button>
+  )
+}
+
+function MakeBoard(event,isFirst){
+  var selectNum = event.target.value;
+  // マス数を変更された場合は各マスの中をnullにする
+  if (isFirst) {
+    var squares = Array(selectNum*selectNum).fill(null);
+  }
+  // 選択されたマス数分のボタンを作成する
+  var mathList = [];
+  for (var i = 0; i < selectNum; i++) {
+    var lineList = [];
+    var k = i*selectNum;
+    for (var j = 0; j < selectNum; j++) {
+      lineList.push(RenderSquare(k+j,squares));
+    }
+    mathList.push(<div className="board-row">{lineList}</div>);
+  }
+  // 作成したボタン情報をstateに状態として更新
+  // this.setState({
+  //   maths: mathList,
+  //   num: selectNum,
+  // });
+  return(
+    mathList
+  );
+}
+
+function usePlayer1IsNext(){ // 独自フック分からん
+  const [player1IsNext, setPlayer1IsNext] = useState(0);
+  useEffect(
+    () => {
+        console.log("hello");
+        setPlayer1IsNext(player1IsNext);
+        console.log(player1IsNext);
+    },
+    [ player1IsNext ]
+  );
+  return(player1IsNext);
+}
+
+function SelectMath(props){
+  // const [player1IsNext, setPlayer1IsNext] = useState(true);
+  const [drawSquare, setDrawSquare] = useState(null);
+  // var status = 'Next player: ' + {player1IsNext} ? props.player1 : props.player2;
+  return(
+    <div>
+      {/* <div className="status">{status}</div> */}
+      <div>
+        {/* 
+          ハンドルイベントのときだけuseStateみたいな使い方はNGっぽい？トップコンポーネントというのは必ず描画されるとこ？
+          独自フックを使うとコンポーネントを跨いで値を使えるっぽいけどたぶんそれもトップコンポーネントのことっぽいかな？
+          classのときはstateで持たせて更新してったけど、これの場合何に持たせれば良いんだ。
+          propsに持たせて毎回引数に入れるのは流石に何か違う気がする。
+         */}
+        <select id="choiceBoard" class="status" onChange={(e) => setDrawSquare(MakeBoard(e, true))}>
+          <option disabled selected>マス目の数を選択してください</option>
+          <option value="3">３×３</option>
+          <option value="4">４×４</option>
+          <option value="5">５×５</option>
+        </select>
+        {/* これが可動式マスの部分 */}
+        {drawSquare}
+      </div>
+    </div>
+  );
+}
+
+function BoardTmp(){
+  const [player1, setPlayer1] = useState(null);
+  const [player2, setPlayer2] = useState(null);
+  return (
+    console.log({player1}),
+    console.log({player2}),
+    <div>
+      <p>プレイヤーの名前を入力してください</p>
+      <div  class="status">
+        <input type="text" placeholder="プレイヤー１の名前を入力" value={player1} onChange={(e) => setPlayer1(e.target.value)}></input>
+        <span> VS </span>
+        <input type="text" placeholder="プレイヤー２の名前を入力" value={player2} onChange={(e) => setPlayer2(e.target.value)}></input>
+      </div>
+      <div>{player1} VS {player2} のゲームです</div>
+      <SelectMath player1={player1} player2={player2}/>
+    </div>
+  );
+}
+
+function RenderBoard(){
+  return <BoardTmp/>;
+}
+
+export default function Game(){
+  return(
+    <div className="game">
+    <div className="game-board" id="nowCreating">
+      <h2>これがfunctionメソッドで統一を図っているもの(途中)</h2>
+      <RenderBoard />
+    </div>
+    <div className="game-info">
+      <div>{/* status */}</div>
+      <ol>{/* TODO */}</ol>
+    </div>
+  </div>
+  );
+}
+
 // ========================================
-// １最初にここが呼ばれる
-ReactDOM.render(
-  <Game />,   // ２これでGameクラスが呼ばれる
-  document.getElementById('root')
-);
-ReactDOM.render(
-  <Index2 />,
-  document.getElementById('root2')
-);
-// htmlにid違う要素をもう一つ書いてこうすれば同じのがもう一個出せる
-ReactDOM.render(
-  <Normal />,
-  document.getElementById('root3')
-);
+// export default Gameaaa;
